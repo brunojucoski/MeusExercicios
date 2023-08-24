993cca2fa4847f30375941591463e87b365bb21f
bruno.jucoski <bruno.vaz.juca@gmail.com>
2023-08-23 22:01:09 -0300

    oba

--- src/exercicioCris/pilhas3.java
+++ src/exercicioCris/pilhas3.java
@@ -9,6 +9,28 @@ public class pilhas3 {
         Stack<Integer> P1 = new Stack<Integer>();
         Stack<Integer> P2 = new Stack<Integer>();
         Scanner scan = new Scanner(System.in);
+        char continuar = 's' ;
+
+        for (int i=0;continuar!= 'n' ; i++) {
+            System.out.println("insira um valor inteiro para preencher a pilha P!: ");
+            int x = scan.nextInt();
+            P1.push(x);
+            System.out.println( " Deseja informar mais um nuemro? : ");
+            continuar = scan.next().charAt(0);
+        }
+
+        for (int i=0; i<5; i++) {
+            System.out.println("insira um valor inteiro para preencher a pilha P2: ");
+            int y = scan.nextInt();
+            P2.push(y);
+            System.out.println(" Deseja informar mais um numero?: ");
+            continuar = scan.next().charAt(0);
+        }
+
+        if (P1=P2)
+
+
+
 
 
 
